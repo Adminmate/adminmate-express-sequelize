d260fe5f8ac9a002689248f76b3e15b2bc62e259
Marc Delalonde <marcdelalonde@gmail.com>
2021-03-24 15:45:07 +0100

    Code refactoring

--- test/general.test.js
+++ test/general.test.js
@@ -20,11 +20,6 @@ const app = require('./app.js');
 // Endpoint prefix
 const prefix = '/adminmate/api';
 
-// Generate the admin token
-const adminToken = jwt.encode({
-  exp_date: Date.now() + 1000
-}, 'authkey_secret');
-
 // Before all
 beforeAll(done => {
   done();
@@ -59,54 +54,6 @@ describe('Testing POST /api/login', () => {
     // Check response
     expect(response.status).toBe(403);
     expect(response.body.message).toBe('Invalid request');
-  });
-});
-
-// Models
-describe('Testing GET /api/models', () => {
-  it('should return a 403 http response', async () => {
-    // Make request
-    const response = await supertest(app)
-      .get(prefix + '/models');
-
-    // Check response
-    expect(response.status).toBe(403);
-    expect(response.body.code).toBe('not_authorized');
-  });
-
-  it('should return a 200 http response', async () => {
-    // Make request
-    const response = await supertest(app)
-      .get(prefix + '/models')
-      .set('x-access-token', adminToken);
-
-    // Check response
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchSnapshot();
-  });
-});
-
-// Models properties
-describe('Testing GET /api/models/properties', () => {
-  it('should return a 403 http response', async () => {
-    // Make request
-    const response = await supertest(app)
-      .get(prefix + '/models/properties');
-
-    // Check response
-    expect(response.status).toBe(403);
-    expect(response.body.code).toBe('not_authorized');
-  });
-
-  it('should return a 200 http response', async () => {
-    // Make request
-    const response = await supertest(app)
-      .get(prefix + '/models/properties')
-      .set('x-access-token', adminToken);
-
-    // Check response
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchSnapshot();
   });
 });
 
