eec453d81162fe05c0efb78b2321f75a3894c013
Marc Delalonde <marcdelalonde@gmail.com>
2021-07-24 19:38:53 +0200

    Model queries enhancements

--- src/controllers/model-query.js
+++ src/controllers/model-query.js
@@ -1,4 +1,26 @@
+const _ = require('lodash');
+const moment = require('moment');
+const { Op } = require('sequelize');
 const fnHelper = require('../helpers/functions');
+
+const getGroupByFieldFormated_SQLite = (sequelizeObject, timerange, groupByDateField) => {
+  switch (timerange) {
+    case 'day': {
+      return sequelizeObject.fn('STRFTIME', '%Y-%m-%d', sequelizeObject.col(groupByDateField));
+    }
+    case 'week': {
+      return sequelizeObject.fn('STRFTIME', '%Y-%W', sequelizeObject.col(groupByDateField));
+    }
+    case 'month': {
+      return sequelizeObject.fn('STRFTIME', '%Y-%m-01', sequelizeObject.col(groupByDateField));
+    }
+    case 'year': {
+      return sequelizeObject.fn('STRFTIME', '%Y-01-01', sequelizeObject.col(groupByDateField));
+    }
+    default:
+      return null;
+  }
+};
 
 module.exports.customQuery = async (req, res) => {
   const data = req.body.data;
@@ -55,82 +77,70 @@ module.exports.customQuery = async (req, res) => {
       res.json({ data: queryData[0].queryResult });
     }
     else {
-      const dataCount = await currentModel.countDocuments({});
+      const dataCount = await currentModel.count({});
       res.json({ data: dataCount });
     }
   }
   else if (data.type === 'bar' || data.type === 'line') {
-    const toSum = data.field && data.operation === 'sum' ? `$${data.field}` : 1;
+    if (!['day', 'week', 'month', 'year'].includes(data.timeframe)) {
+      return res.status(403).json();
+    }
 
     let matchReq = {};
-    let groupFormat = '';
 
     // Day timeframe
     if (data.timeframe === 'day') {
       matchReq = {
-        '$gte': new Date(global._moment().subtract(30, 'day').startOf('day').format()),
-        '$lte': new Date(global._moment().endOf('day').format())
+        [Op.gte]: new Date(moment().subtract(30, 'day').startOf('day').format()),
+        [Op.lte]: new Date(moment().endOf('day').format())
       };
-      groupFormat = '%Y-%m-%d';
     }
     // Week timeframe
     else if (data.timeframe === 'week') {
       matchReq = {
-        '$gte': new Date(global._moment().subtract(26, 'week').startOf('week').format()),
-        '$lte': new Date(global._moment().endOf('week').format())
+        [Op.gte]: new Date(moment().subtract(26, 'week').startOf('week').format()),
+        [Op.lte]: new Date(moment().endOf('week').format())
       };
-      groupFormat = '%V';
     }
     // Month timeframe
     else if (data.timeframe === 'month') {
       matchReq = {
-        '$gte': new Date(global._moment().subtract(12, 'month').startOf('month').format()),
-        '$lte': new Date(global._moment().endOf('month').format())
+        [Op.gte]: new Date(moment().subtract(12, 'month').startOf('month').format()),
+        [Op.lte]: new Date(moment().endOf('month').format())
       };
-      groupFormat = '%m';
     }
     // Year timeframe
     else if (data.timeframe === 'year') {
       matchReq = {
-        '$gte': new Date(global._moment().subtract(8, 'year').startOf('year').format()),
-        '$lte': new Date(global._moment().endOf('year').format())
+        [Op.gte]: new Date(moment().subtract(8, 'year').startOf('year').format()),
+        [Op.lte]: new Date(moment().endOf('year').format())
       };
-      groupFormat = '%Y';
     }
 
-    if (!groupFormat) {
-      return res.status(403).json({ message: 'Invalid request' });
-    }
-
+    // Request
     const repartitionData = await currentModel
-      .aggregate([
-        {
-          $match: {
-            [data.group_by]: matchReq
-          }
+      .findAll({
+        attributes: [
+          [ getGroupByFieldFormated_SQLite(currentModel.sequelize, data.timeframe, data.group_by), 'key' ],
+          [ currentModel.sequelize.fn(data.operation, currentModel.sequelize.col(data.field)), 'value' ]
+        ],
+        group: 'key',
+        where: {
+          [data.group_by]: matchReq
         },
-        {
-          $group: {
-            _id: { $dateToString: { format: groupFormat, date: `$${data.group_by}` } },
-            count: { $sum: toSum }
-          }
-        },
-        {
-          $project: {
-            key: '$_id',
-            value: '$count',
-            _id: false
-          }
-        }
-      ]);
+        raw: true
+      });
+
+    console.log('=========repartitionData', repartitionData);
 
     const formattedData = [];
 
     // Day timeframe
     if (data.timeframe === 'day') {
       for (let i = 0; i < 30; i++) {
-        const currentDate = global._moment().subtract(i, 'day');
-        const countForTheTimeframe = global._.find(repartitionData, { key: currentDate.format('YYYY-MM-DD') });
+        const currentDate = moment().subtract(i, 'day');
+
+        const countForTheTimeframe = _.find(repartitionData, { key: currentDate.format('YYYY-MM-DD') });
         formattedData.push({
           key: currentDate.format('DD/MM'),
           value: countForTheTimeframe ? countForTheTimeframe.value : 0
@@ -140,9 +150,9 @@ module.exports.customQuery = async (req, res) => {
     // Week timeframe
     else if (data.timeframe === 'week') {
       for (let i = 0; i < 26; i++) {
-        const currentWeek = global._moment().subtract(i, 'week');
+        const currentWeek = moment().subtract(i, 'week');
 
-        const countForTheTimeframe = global._.find(repartitionData, { key: currentWeek.format('WW') });
+        const countForTheTimeframe = _.find(repartitionData, { key: currentWeek.format('YYYY-WW') });
         formattedData.push({
           key: currentWeek.startOf('week').format('DD/MM'),
           value: countForTheTimeframe ? countForTheTimeframe.value : 0
@@ -152,9 +162,9 @@ module.exports.customQuery = async (req, res) => {
     // Month timeframe
     else if (data.timeframe === 'month') {
       for (let i = 0; i < 12; i++) {
-        const currentMonth = global._moment().subtract(i, 'month');
+        const currentMonth = moment().subtract(i, 'month');
 
-        const countForTheTimeframe = global._.find(repartitionData, { key: currentMonth.format('MM') });
+        const countForTheTimeframe = _.find(repartitionData, { key: currentMonth.format('MM') });
         formattedData.push({
           key: currentMonth.startOf('month').format('MMM'),
           value: countForTheTimeframe ? countForTheTimeframe.value : 0
@@ -164,9 +174,9 @@ module.exports.customQuery = async (req, res) => {
     // Year timeframe
     else if (data.timeframe === 'year') {
       for (let i = 0; i < 8; i++) {
-        const currentYear = global._moment().subtract(i, 'year');
+        const currentYear = moment().subtract(i, 'year');
 
-        const countForTheTimeframe = global._.find(repartitionData, { key: currentYear.format('YYYY') });
+        const countForTheTimeframe = _.find(repartitionData, { key: currentYear.format('YYYY') });
         formattedData.push({
           key: currentYear.startOf('year').format('YYYY'),
           value: countForTheTimeframe ? countForTheTimeframe.value : 0
