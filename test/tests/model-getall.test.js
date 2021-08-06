f20f48fbd447a0721e6b92e68a29212c5d45721a
Marc Delalonde <marcdelalonde@gmail.com>
2021-08-06 15:02:03 +0200

    Tests enhancements

--- test/tests/model-getall.test.js
+++ test/tests/model-getall.test.js
@@ -60,4 +60,97 @@ describe('Cars request', () => {
     expect(response.statusCode).toBe(200);
     expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
   });
+
+  it('- With "fields" parameter (name & manufacturer only)', async () => {
+    const request = makeCarsReq({
+      fields: ['name', 'manufacturer']
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
+
+  it('- With "page" parameter set to 2', async () => {
+    const request = makeCarsReq({
+      page: 2
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
+
+  it('- With a "search" parameter', async () => {
+    const request = makeCarsReq({
+      fields: ['name'],
+      search: 'Porsche 91'
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
+
+  it('- With a "order" parameter', async () => {
+    const request = makeCarsReq({
+      fields: ['name'],
+      search: 'Porsche 91',
+      order: [['name', 'ASC']]
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
+
+  it('- With a "filters" parameter', async () => {
+    const request = makeCarsReq({
+      fields: ['name', 'year'],
+      search: 'Porsche',
+      filters: {
+        operator: 'or',
+        list: [
+          { field: 'year', operator: 'is', value: 1968 },
+          { field: 'year', operator: 'is', value: 1969 }
+        ]
+      }
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
+
+  it('- With a "segment" parameter', async () => {
+    const request = makeCarsReq({
+      fields: ['name', 'year'],
+      segment: {
+        type: 'code',
+        data: 'ferrari'
+      }
+    });
+
+    const response = httpMocks.createResponse();
+    await getAll(request, response, (err) => expect(err).toBeFalsy());
+
+    const responseData = response._getJSONData();
+    expect(response.statusCode).toBe(200);
+    expect(responseData).toMatchSpecificSnapshot('./__snapshots__/model-getall.shot');
+  });
 });
