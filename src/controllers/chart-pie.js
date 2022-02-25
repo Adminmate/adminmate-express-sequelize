ca92b99144c23d6e27dfefdc0c2e621f14d2c341
Marc Delalonde <marcdelalonde@gmail.com>
2022-02-25 11:34:36 +0100

    Chart-pie enhancements

--- src/controllers/chart-pie.js
+++ src/controllers/chart-pie.js
@@ -1,25 +1,63 @@
+const Joi = require('@hapi/joi');
+
 module.exports = async (currentModel, data) => {
   const sequelizeInstance = currentModel.sequelize;
-  const seqFnField = data.field ? sequelizeInstance.col(data.field) : 1;
 
-  // Query database
-  const repartitionData = await currentModel.findAll({
-    attributes: [
-      [ data.group_by, 'key' ],
-      [ sequelizeInstance.fn(data.operation, seqFnField), 'value' ]
-    ],
-    group: ['key'],
-    raw: true
+  const paramsSchema = Joi.object({
+    type: Joi.string().required(),
+    model: Joi.string().required(),
+    operation: Joi.string().required(),
+    group_by: Joi.string().required(),
+    field: Joi.alternatives().conditional('operation', {
+      not: 'count',
+      then: Joi.string().required(),
+      otherwise: Joi.string()
+    }),
+    limit: Joi.number().optional()
   });
 
-  // To make sure value attribute is an integer/float
-  const cleanData = repartitionData.map(data => ({ ...data, value: parseFloat(data.value) }));
+  // Validate params
+  const { error } = paramsSchema.validate(data);
+  if (error) {
+    return {
+      success: false,
+      message: error.details[0].message
+    };
+  }
 
-  return {
-    success: true,
-    data: {
-      config: null,
-      data: cleanData
-    }
-  };
+  try {
+    const seqFnField = data.field ? sequelizeInstance.col(data.field) : 1;
+
+    // Query database
+    const repartitionData = await currentModel.findAll({
+      attributes: [
+        [ data.group_by, 'key' ],
+        [ sequelizeInstance.fn(data.operation, seqFnField), 'value' ]
+      ],
+      group: ['key'],
+      order: sequelizeInstance.literal('value DESC'),
+      limit: data.limit || 15,
+      raw: true
+    });
+
+    // To make sure value attribute is an integer/float
+    const cleanData = repartitionData.map(data => ({
+      ...data,
+      value: parseFloat(data.value)
+    }));
+
+    return {
+      success: true,
+      data: {
+        config: null,
+        data: cleanData
+      }
+    };
+  }
+  catch(e) {
+    return {
+      success: false,
+      message: e.message
+    };
+  }
 };
